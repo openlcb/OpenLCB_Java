ba782757a0be4701f6e9b2f059aff2381d8ebf99
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2009-08-20 04:49:52 +0000

    fix conflict. add test that gound it
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/s96/trunk/prototypes/java@35 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- src/org/nmra/net/IdentifyConsumersMessage.java
+++ src/org/nmra/net/IdentifyConsumersMessage.java
@@ -8,10 +8,12 @@ package org.nmra.net;
  */
 public class IdentifyConsumersMessage extends Message {
     
-    public IdentifyConsumersMessage(NodeID source) {
+    public IdentifyConsumersMessage(NodeID source, EventID event) {
         super(source);
+        this.eventID = event;
     }
-        
+    
+    EventID eventID = null;
     /**
      * Implement message-type-specific
      * processing when this message
@@ -23,4 +25,22 @@ public class IdentifyConsumersMessage extends Message {
      public void applyTo(MessageDecoder decoder, Connection sender) {
         decoder.handleIdentifyConsumers(this, sender);
      }
+
+     /**
+      * To be equal, messages have to have the
+      * same type and content
+      */
+     @Override
+     public boolean equals(Object o) {
+        if (!super.equals(o)) return false; // also checks type
+        IdentifyConsumersMessage msg = (IdentifyConsumersMessage) o;
+        if (! this.eventID.equals(msg.eventID))
+            return false;
+        return true;
+     }
+
+    public String toString() {
+        return getSourceNodeID().toString()
+                +" Identify Consumers with "+eventID.toString();     
+    }
 }
