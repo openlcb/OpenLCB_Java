41c401b8285fffdb64a2146ac1528107c56ec66e
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2010-09-27 19:42:20 +0000

    improve internal structure
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@962 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- src/org/openlcb/can/MessageBuilder.java
+++ src/org/openlcb/can/MessageBuilder.java
@@ -9,21 +9,34 @@ import java.util.List;
  * and vice-versa.
  *<p>
  * In general, the connection is one-to-one, but
- * sometimes (datagram expansion) more than one will be
+ * sometimes (datagram expansion) more than one frame will be
  * created.
- * The List is always returned, even if empty.
  * 
  * @author  Bob Jacobsen   Copyright 2010
  * @version $Revision$
  */
 public class MessageBuilder {
 
+    /**
+     * The provided AliasMap will be updated
+     * as inbound frames are processed.
+     */
     public MessageBuilder(AliasMap map) {
         this.map = map;
     }
 
     AliasMap map;
     
+    /** 
+     * Accept a frame, and convert to 
+     * a standard OpenLCB Message object.
+     *
+     * The returned Message is fully initialized
+     * with content, and the original frame can 
+     * be dropped.
+     *
+     * The List is always returned, even if empty.
+     */
     public List<Message> processFrame(OpenLcbCanFrame f) {
         List<Message> retlist = new java.util.ArrayList<Message>();
         NodeID source = map.getNodeID(f.getSourceAlias());
@@ -90,38 +103,155 @@ public class MessageBuilder {
         return retlist;
     }
 
-    public List<OpenLcbCanFrame> processMessage(Message f) {
-        List<OpenLcbCanFrame> retlist = new java.util.ArrayList<OpenLcbCanFrame>();
-        switch(f.getMTI()) {
+    /** 
+     * Accept an OpenLCB Message, and convert to 
+     * a standard frame object.
+     *
+     * The returned frame(s) are fully initialized
+     * with content, and the original frame can 
+     * be dropped.
+     *
+     * The List is always returned, and should never be empty.
+     */
+    public List<OpenLcbCanFrame> processMessage(Message msg) {
 
-        case OpenLcb.MTI_INITIALIZATION_COMPLETE:
-
-        case OpenLcb.MTI_VERIFY_NID:
-        case OpenLcb.MTI_VERIFIED_NID:      // also 30B2 
-        
-        case OpenLcb.MTI_IDENTIFY_CONSUMERS:
-        case OpenLcb.MTI_IDENTIFY_CONSUMERS_RANGE:
-        case OpenLcb.MTI_CONSUMER_IDENTIFIED:
-        
-        case OpenLcb.MTI_IDENTIFY_PRODUCERS:
-        case OpenLcb.MTI_IDENTIFY_PRODUCERS_RANGE:
-        case OpenLcb.MTI_PRODUCER_IDENTIFIED:
-        
-        case OpenLcb.MTI_IDENTIFY_EVENTS:   // also 32B2
-        
-        case OpenLcb.MTI_LEARN_EVENT:
-        case OpenLcb.MTI_PC_EVENT_REPORT:
-        
-        case OpenLcb.MTI_DATAGRAM:
-        case OpenLcb.MTI_DATAGRAM_RCV_OK:
-        case OpenLcb.MTI_DATAGRAM_REJECTED:
+        FrameBuilder f = new FrameBuilder();
+        return f.convert(msg);
+    }
     
-        case OpenLcb.MTI_STREAM_INIT_REQUEST:
-        case OpenLcb.MTI_STREAM_INIT_REPLY:
-        case OpenLcb.MTI_STREAM_DATA_SEND:
-        case OpenLcb.MTI_STREAM_DATA_PROCEED:
-        case OpenLcb.MTI_STREAM_DATA_COMPLETE:
+    private class FrameBuilder extends org.openlcb.MessageDecoder {
+        /**
+         * Catches messages that are not explicitly 
+         * handled and throws an error
+         */
+        protected void defaultHandler(Message msg, Connection sender) {
+            throw new java.lang.NoSuchMethodError("no handler for Message: "+msg.toString());
         }
-        return retlist;
+        
+        List<OpenLcbCanFrame> retlist;
+        
+        List<OpenLcbCanFrame> convert(Message msg) {
+            retlist = new java.util.ArrayList<OpenLcbCanFrame>();
+            
+            // Uses the double dispatch mechanism built into Message
+            put(msg, null);  // no Connection needed
+            
+            return retlist;
+        }
+                
+        /**
+         * Handle "Initialization Complete" message
+         */
+        public void handleInitializationComplete(InitializationCompleteMessage msg, Connection sender){
+            defaultHandler(msg, sender);
+        }
+        /**
+         * Handle "Verified Node ID Number" message
+         */
+        public void handleVerifiedNodeIDNumber(VerifiedNodeIDNumberMessage msg, Connection sender){
+            defaultHandler(msg, sender);
+        }
+        /**
+         * Handle "Verify Node ID Number" message
+         */
+        public void handleVerifyNodeIDNumber(VerifyNodeIDNumberMessage msg, Connection sender){
+            OpenLcbCanFrame f = new OpenLcbCanFrame(0x00);
+            f.setVerifiedNID(msg.getSourceNodeID());
+            f.setSourceAlias(map.getAlias(msg.getSourceNodeID()));
+            retlist.add(f);
+        }
+        /**
+         * Handle "Producer/Consumer Event Report" message
+         */
+        public void handleProducerConsumerEventReport(ProducerConsumerEventReportMessage msg, Connection sender){
+            OpenLcbCanFrame f = new OpenLcbCanFrame(0x00);
+            f.setPCEventReport(msg.getEventID());
+            f.setSourceAlias(map.getAlias(msg.getSourceNodeID()));
+            retlist.add(f);
+        }
+        /**
+         * Handle "Identify Consumers" message
+         */
+        public void handleIdentifyConsumers(IdentifyConsumersMessage msg, Connection sender){
+            defaultHandler(msg, sender);
+        }
+        /**
+         * Handle "Consumer Identified" message
+         */
+        public void handleConsumerIdentified(ConsumerIdentifiedMessage msg, Connection sender){
+            defaultHandler(msg, sender);
+        }
+        /**
+         * Handle "Identify Producers" message
+         */
+        public void handleIdentifyProducers(IdentifyProducersMessage msg, Connection sender){
+            defaultHandler(msg, sender);
+        }
+        /**
+         * Handle "Producer Identified" message
+         */
+        public void handleProducerIdentified(ProducerIdentifiedMessage msg, Connection sender){
+            defaultHandler(msg, sender);
+        }
+        /**
+         * Handle "Identify Event" message
+         */
+        public void handleIdentifyEvents(IdentifyEventsMessage msg, Connection sender){
+            defaultHandler(msg, sender);
+        }
+        /**
+         * Handle "Learn Event" message
+         */
+        public void handleLearnEvent(LearnEventMessage msg, Connection sender){
+            defaultHandler(msg, sender);
+        }
+        /**
+         * Handle "Datagram" message
+         */
+        public void handleDatagram(DatagramMessage msg, Connection sender){
+            defaultHandler(msg, sender);
+        }
+        /**
+         * Handle "Datagram Rejected" message
+         */
+        public void handleDatagramRejected(DatagramRejectedMessage msg, Connection sender){
+            defaultHandler(msg, sender);
+        }
+        /**
+         * Handle "Datagram Acknowledged" message
+         */
+        public void handleDatagramAcknowledged(DatagramAcknowledgedMessage msg, Connection sender){
+            defaultHandler(msg, sender);
+        }
+        /**
+         * Handle "Stream Init Request" message
+         */
+        public void handleStreamInitRequest(StreamInitRequestMessage msg, Connection sender){
+            defaultHandler(msg, sender);
+        }
+        /**
+         * Handle "Stream Init Reply" message
+         */
+        public void handleStreamInitReply(StreamInitReplyMessage msg, Connection sender){
+            defaultHandler(msg, sender);
+        }
+        /**
+         * Handle "Stream Data Send" message
+         */
+        public void handleStreamDataSend(StreamDataSendMessage msg, Connection sender){
+            defaultHandler(msg, sender);
+        }
+        /**
+         * Handle "Stream Data Proceed" message
+         */
+        public void handleStreamDataProceed(StreamDataProceedMessage msg, Connection sender){
+            defaultHandler(msg, sender);
+        }
+        /**
+         * Handle "Stream Data Complete" message
+         */
+        public void handleStreamDataComplete(StreamDataCompleteMessage msg, Connection sender){
+            defaultHandler(msg, sender);
+        }
     }
  }
