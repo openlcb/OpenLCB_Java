feb00d2f3048e459a468070370591a2fe0fd9e43
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2011-03-20 17:28:58 +0000

    prefer package-info.java to package.html
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@1154 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

---
+++ test/simulations/package-info.java
@@ -0,0 +1,10 @@
+/**
+
+Stand-alone simulations for OpenLCB development.
+
+<p>
+This is a stand-alone package for small simulations and calculations.
+It should not contains parts of the sample implementation.
+
+*/
+package simulations;
