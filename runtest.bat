a57c878c6e906eba085b6c88dc93c55f9aa2f3cf
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2011-03-20 20:39:39 +0000

    add icon library
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@1158 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- runtest.bat
+++ runtest.bat
@@ -1,1 +1,1 @@
-java -noverify -Dsun.java2d.noddraw -Djava.library.path=.:lib/ -cp .;classes;lib/junit.jar;lib/jdom.jar $1 $2 $3
+java -noverify -Dsun.java2d.noddraw -Djava.library.path=.:lib/ -cp .;classes;lib/junit.jar;lib/jdom.jar;lib/jlfgr-1_0.jar $1 $2 $3
