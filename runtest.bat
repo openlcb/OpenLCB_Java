5bfe18555282ec456512cf8bdd5e98a9098eedb9
Bob Jacobsen <jacobsen@mac.com>
2011-03-20 05:07:45 +0000

    add JDOM
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@1140 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- runtest.bat
+++ runtest.bat
@@ -1,1 +1,1 @@
-java -noverify -Dsun.java2d.noddraw -Djava.library.path=.:lib/ -cp .;classes;lib/junit.jar $1 $2 $3
+java -noverify -Dsun.java2d.noddraw -Djava.library.path=.:lib/ -cp .;classes;lib/junit.jar;lib/jdom.jar $1 $2 $3
