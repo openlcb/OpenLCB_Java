e66bb6d79a6f51a178ac44cc243de6e0e453c589
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2011-03-20 17:27:38 +0000

    prefer package-info.java to package.html
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@1153 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

---
+++ test/scenarios/can/package-info.java
@@ -0,0 +1,11 @@
+/**
+
+Directory of high-level tests and examples for OpenLCB CAN wire protocol.
+<p>
+Unit tests are kept in the same package as the code being tested.
+This package contains tests that involve the interaction of
+multiple classes to implement nodes, links, gateways, etc,
+in the CAN wire protocol.
+
+*/
+package scenarios.can;
