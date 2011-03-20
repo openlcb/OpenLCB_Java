059b449ed5fa6a9d66f902c6be9a8c8977d68b1e
Bob Jacobsen <jacobsen@mac.com>
2011-03-20 17:26:17 +0000

    prefer package-info.java to package.html
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@1152 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

---
+++ test/tools/cansim/package-info.java
@@ -0,0 +1,14 @@
+/**
+
+Provides a simple CAN Bus simulator for testing CAN-specific algorithms.
+
+<h2>Related Documentation</h2>
+
+(Pointers to CAN info)
+
+<!-- @since 1.2.3 -->
+
+@see org.openlcb.can
+
+*/
+package tools.cansim;
