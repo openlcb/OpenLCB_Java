ccb2e08dcc8eb6533473601b02881488cab0f75c
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2011-03-20 17:17:10 +0000

    prefer package-info.java to package.html
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@1148 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

---
+++ package-info-template.java
@@ -0,0 +1,28 @@
+
+/**
+Sample package-info.java file.  The first sentence is extracted and used in the TOC.
+<p>
+This is a place to provide PACKAGE-level annotations, see example below.
+<p>
+Note that JavaDoc will use this file in preference to the package.html file for 
+package-level comments.  The format is slightly different, because the HTML
+boilerplate isn't needed, and you don't need to start each line with "*".
+<p>
+For more info on use of this file, see:
+<ul>
+    <li><a href="http://onjava.com/pub/a/onjava/2004/04/21/declarative.html?page=3">http://onjava.com/pub/a/onjava/2004/04/21/declarative.html?page=3</a>
+    <li><a href="http://java.sun.com/docs/books/jls/third_edition/html/packages.html">http://java.sun.com/docs/books/jls/third_edition/html/packages.html</a>
+    <li><a href="http://www.javapractices.com/topic/TopicAction.do?Id=60">http://www.javapractices.com/topic/TopicAction.do?Id=60</a>
+</ul>
+<p>
+For more JavaDoc info, see
+<ul>
+    <li><a href="http://download.oracle.com/javase/1.5.0/docs/tooldocs/windows/javadoc.html">http://download.oracle.com/javase/1.5.0/docs/tooldocs/windows/javadoc.html</a>
+</ul>
+
+@since 0.1
+@see org.openlcb.Node
+
+ */
+@edu.umd.cs.findbugs.annotations.SuppressWarnings({"EI_EXPOSE_REP2", "MS_OOI_PKGPROTECT", "MS_PKGPROTECT", "NM_METHOD_NAMING_CONVENTION", "SBSC_USE_STRINGBUFFER_CONCATENATION"} )
+package jmri.jmris.srcp.parser;
