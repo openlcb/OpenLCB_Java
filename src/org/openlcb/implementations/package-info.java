3815555aee52e9597a1d785f4d49c0b488f903f1
Bob Jacobsen <jacobsen@mac.com>
2011-03-20 17:13:00 +0000

    prefer package-info.java
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@1144 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

---
+++ src/org/openlcb/implementations/package-info.java
@@ -0,0 +1,9 @@
+/**
+Provides sample implementations for OpenLCB Java interfaces.
+<p>
+These are somewhere between
+functional tests and common code intended for reuse.
+They do useful things, but are probably not useful by
+themselves.
+*/
+package org.openlcb.implementations;
