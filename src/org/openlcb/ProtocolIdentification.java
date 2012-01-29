33c810353360b3880f0a6f3df695628a31520c90
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2012-01-29 07:35:53 +0000

    add ProtocolIdentificationProtocol decode
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@1688 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

---
+++ src/org/openlcb/ProtocolIdentification.java
@@ -0,0 +1,46 @@
+// ProtocolIdentification.java
+
+package org.openlcb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Protocol Identification Protocol 
+ *
+ * @see             http://www.openlcb.org/trunk/specs/drafts/GenProtocolIdS.pdf
+ * @author			Bob Jacobsen   Copyright (C) 2010
+ * @version			$Revision: 18542 $
+ *
+ */
+public class ProtocolIdentification {
+
+    public enum Protocols {
+        ProtocolIdentification( 0x800000000000L,"ProtocolIdentification"), 
+        Datagram(               0x400000000000L,"Datagram"),
+        Stream(                 0x200000000000L,"Stream"), 
+        Configuration(          0x100000000000L,"Configuration"),
+        Reservation(            0x080000000000L,"Reservation"),
+        Display(                0x040000000000L,"Display"),
+        Identification(         0x020000000000L,"Identification"),
+        TeachingLearningConfiguration(0x010000000000L,"TeachingLearningConfiguration"),
+        RemoteButton(           0x008000000000L,"RemoteButton"),
+        AbbreviatedDefaultCDI(  0x004000000000L,"AbbreviatedDefaultCDI");
+        
+        Protocols(long value, String name) {
+            this.value = value;
+            this.name = name;
+        }
+        long value;
+        String name;
+        
+        static List<String> decode(long r) {
+            ArrayList<String> retval = new ArrayList<String>();
+            for (Protocols t : Protocols.values()) {
+                if ( (t.value & r) != 0 ) retval.add(t.name);
+            }
+            return retval;
+        }
+    }
+        
+}
