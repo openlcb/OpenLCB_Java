df738fff590c8ab07893c6f96b820c64e39be8bd
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2012-03-14 19:43:19 +0000

    complete 1st pass
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@2017 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

---
+++ src/org/openlcb/implementations/DatagramService.java
@@ -0,0 +1,162 @@
+package org.openlcb.implementations;
+
+import org.openlcb.*;
+
+// For annotations
+import net.jcip.annotations.*; 
+import edu.umd.cs.findbugs.annotations.*; 
+
+/**
+ * Service for sending and receiving data via datagrams.
+ * 
+ * Meant to shield the using code from all the details of that
+ * process via some primitives:
+ * <ul>
+ * <li>Send data via a memo that gets notified on success
+ * <li>Register to be notified when data of a particular type arrives
+ * </ul>
+ *
+ *
+ * @author  Bob Jacobsen   Copyright 2012
+ * @version $Revision$
+ */
+public class DatagramService extends MessageDecoder {
+
+    /**
+     * @param downstream Connection in the direction of the layout
+     */
+    public DatagramService(NodeID here, Connection downstream) {
+        this.here = here;
+        this.downstream = downstream;
+        
+    }
+    
+    static final int DEFAULT_ERROR_CODE = 0x1000;
+    NodeID here;
+    Connection downstream;
+
+    /**
+     * Send data to layout
+     */
+    public void sendData(DatagramServiceTransmitMemo memo){
+        xmtMemo = memo;
+        Message m = new DatagramMessage(here, memo.dest, memo.data);
+        downstream.put(m, this);
+    }
+
+    /**
+     * Handle "Datagram" message from layout
+     */
+    public void handleDatagram(DatagramMessage msg, Connection sender){
+        // forward
+        int retval = DEFAULT_ERROR_CODE;
+        if (rcvMemo != null && rcvMemo.type == msg.getData()[0]) {
+            retval = rcvMemo.handleData(msg.getData());
+        }
+        if (retval  == 0) {
+            // accept
+            Message m = new DatagramAcknowledgedMessage(here, msg.getSourceNodeID());
+            downstream.put(m, this);
+        } else {
+            // reject
+            Message m = new DatagramRejectedMessage(here, msg.getSourceNodeID(), retval);
+            downstream.put(m, this);
+        }
+    }
+
+    /**
+     * Handle positive datagram reply message from layout
+     */
+    public void handleDatagramAcknowledged(DatagramAcknowledgedMessage msg, Connection sender){
+        if (xmtMemo != null) {
+            xmtMemo.handleReply(0);
+        }
+        xmtMemo = null;
+    }
+
+    DatagramServiceReceiveMemo rcvMemo;
+    DatagramServiceTransmitMemo xmtMemo;
+    
+    /**
+     * Accept request to notify for a particular
+     * type of datagram
+     */
+    public void registerForReceive(DatagramServiceReceiveMemo memo) {
+        this.rcvMemo = memo;
+    }
+    
+    @Immutable
+    @ThreadSafe    
+    static protected class DatagramServiceReceiveMemo {
+        public DatagramServiceReceiveMemo(int type) {
+            this.type = type;
+        }
+
+        int type;
+        
+        public boolean equals(Object o) {
+            if (o == null) return false;
+            if (! (o instanceof DatagramServiceReceiveMemo)) return false;
+            return this.type == ((DatagramServiceReceiveMemo)o).type;
+        } 
+    
+        public String toString() {
+            return "DatagramServiceReceiveMemo: "+type;
+        }
+        
+        public int hashCode() { return type; }
+        
+        /**
+         * Overload this to for notification of data.
+         * @returns 0 for OK, non-zero for error reply
+         */
+        public int handleData(int[] data) { 
+            // default is error
+            return DEFAULT_ERROR_CODE; 
+        }
+
+    }
+
+    @Immutable
+    @ThreadSafe    
+    static protected class DatagramServiceTransmitMemo {
+        public DatagramServiceTransmitMemo(NodeID dest, int[] data) {
+            this.data = data;
+            this.dest = dest;
+        }
+
+        protected DatagramServiceTransmitMemo(NodeID dest) {
+            this.data = data;
+            this.dest = dest;
+        }
+
+        protected int[] data;
+        NodeID dest;
+        
+        public boolean equals(Object o) {
+            if (o == null) return false;
+            if (! (o instanceof DatagramServiceTransmitMemo)) return false;
+            DatagramServiceTransmitMemo m = (DatagramServiceTransmitMemo) o;
+            if (this.data.length != m.data.length) return false;
+            if (this.dest != m.dest) return false;
+            for (int i = 0; i < this.data.length; i++)
+                if (this.data[i] != m.data[i]) return false;
+            return true;
+        } 
+    
+        public String toString() {
+            return "DatagramServiceTransmitMemo: "+data;
+        }
+        
+        public int hashCode() { return this.data.length+this.data[0]+dest.hashCode(); }
+        
+        /**
+         * Overload this to for notification of response
+         * @param code 0 for OK, non-zero for error reply
+         */
+        public void handleReply(int code) { 
+        }
+
+    }
+    
+}
---
+++ test/org/openlcb/implementations/DatagramServiceTest.java
@@ -0,0 +1,187 @@
+package org.openlcb.implementations;
+
+import org.openlcb.*;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * @author  Bob Jacobsen   Copyright 2012
+ * @version $Revision$
+ */
+public class DatagramServiceTest extends TestCase {
+    
+    NodeID hereID = new NodeID(new byte[]{1,2,3,4,5,6});
+    NodeID farID = new NodeID(new byte[]{1,2,3,4,5,7});
+    Connection testConnection;
+    java.util.ArrayList<Message> messagesReceived;
+    boolean flag;
+    DatagramService service;
+    
+    public void setUp() {
+        messagesReceived = new java.util.ArrayList<Message>();
+        testConnection = new Connection(){
+            public void put(Message msg, Connection sender) {
+                messagesReceived.add(msg);
+            }
+        };
+        service = new DatagramService(hereID, testConnection);
+        flag = false;
+    }
+    
+    
+    
+    public void testCtorViaSetup() {
+    }
+    
+    public void testRcvMemoIsRealClass() {
+        DatagramService.DatagramServiceReceiveMemo m20 = 
+            new DatagramService.DatagramServiceReceiveMemo(0x20);
+        DatagramService.DatagramServiceReceiveMemo m21 = 
+            new DatagramService.DatagramServiceReceiveMemo(0x21);
+        
+        Assert.assertTrue(m20.equals(m20));
+        Assert.assertTrue(!m20.equals(null));
+        Assert.assertTrue(!m20.equals(m21));
+        
+    }
+
+    public void testXmtMemoIsRealClass() {
+        DatagramService.DatagramServiceTransmitMemo m20 = 
+            new DatagramService.DatagramServiceTransmitMemo(farID,new int[]{1});
+        DatagramService.DatagramServiceTransmitMemo m21 = 
+            new DatagramService.DatagramServiceTransmitMemo(farID,new int[]{1});
+        DatagramService.DatagramServiceTransmitMemo m22 = 
+            new DatagramService.DatagramServiceTransmitMemo(farID,new int[]{2});
+        DatagramService.DatagramServiceTransmitMemo m23 = 
+            new DatagramService.DatagramServiceTransmitMemo(farID,new int[]{1,2});
+        
+        Assert.assertTrue(m20.equals(m20));
+        Assert.assertTrue(m20.equals(m21));
+        Assert.assertTrue(!m20.equals(null));
+        Assert.assertTrue(!m20.equals(m22));
+        Assert.assertTrue(!m20.equals(m23));
+        Assert.assertTrue(!m22.equals(m20));
+        Assert.assertTrue(!m23.equals(m20));
+        
+    }
+
+    public void testRegisterForData() {
+        DatagramService.DatagramServiceReceiveMemo m20 = 
+            new DatagramService.DatagramServiceReceiveMemo(0x20);
+
+        service.registerForReceive(m20);        
+    }
+
+    public void testReceiveDGbeforeReg() {
+        DatagramService.DatagramServiceReceiveMemo m20 = 
+            new DatagramService.DatagramServiceReceiveMemo(0x20){
+                public int handleData(int[] data) { 
+                    flag = true;
+                    return 0; 
+                }
+            };
+        
+        Message m = new DatagramMessage(farID, hereID, new int[]{0x020});
+      
+        Assert.assertEquals(0,messagesReceived.size());
+
+        Assert.assertTrue(!flag);
+        service.put(m, null);
+        Assert.assertTrue(!flag);
+        
+        Assert.assertEquals(1,messagesReceived.size());
+        Assert.assertTrue(messagesReceived.get(0) instanceof DatagramRejectedMessage);
+    }
+
+    public void testReceiveFirstDG() {
+        DatagramService.DatagramServiceReceiveMemo m20 = 
+            new DatagramService.DatagramServiceReceiveMemo(0x20){
+                public int handleData(int[] data) { 
+                    flag = true;
+                    return 0; 
+                }
+            };
+
+        service.registerForReceive(m20);  
+        
+        Message m = new DatagramMessage(farID, hereID, new int[]{0x020});
+      
+        Assert.assertTrue(!flag);
+        service.put(m, null);
+        Assert.assertTrue(flag);
+        
+        Assert.assertEquals(1,messagesReceived.size());
+        Assert.assertTrue(messagesReceived.get(0) instanceof DatagramAcknowledgedMessage);
+    }
+
+    public void testReceiveWrongDGType() {
+        DatagramService.DatagramServiceReceiveMemo m20 = 
+            new DatagramService.DatagramServiceReceiveMemo(0x20){
+                public int handleData(int[] data) { 
+                    flag = true;
+                    return 0; 
+                }
+            };
+
+        service.registerForReceive(m20);  
+        
+        Message m = new DatagramMessage(farID, hereID, new int[]{0x21});
+      
+        Assert.assertTrue(!flag);
+        service.put(m, null);
+        Assert.assertTrue(!flag);
+        
+        Assert.assertEquals(1,messagesReceived.size());
+        Assert.assertTrue(messagesReceived.get(0) instanceof DatagramRejectedMessage);
+    }
+
+
+    public void testSendOK() {
+        int[] data = new int[]{1,2,3,4,5};
+        DatagramService.DatagramServiceTransmitMemo memo = 
+            new DatagramService.DatagramServiceTransmitMemo(farID,data) {
+                public void handleReply(int code) { 
+                    flag = true;
+                }
+            };
+            
+        service.sendData(memo);
+        
+        Assert.assertEquals("init messages", 1, messagesReceived.size());
+        Assert.assertTrue(messagesReceived.get(0)
+                           .equals(new DatagramMessage(hereID, farID, data)));
+
+        // Accepted
+        Message m = new DatagramAcknowledgedMessage(farID, hereID);
+        messagesReceived = new java.util.ArrayList<Message>();
+
+        Assert.assertTrue(!flag);
+        service.put(m, null);
+        Assert.assertTrue(flag);
+
+        Assert.assertEquals("1st messages", 0, messagesReceived.size());
+        
+    }
+    
+    
+    // from here down is testing infrastructure
+    
+    public DatagramServiceTest(String s) {
+        super(s);
+    }
+
+    // Main entry point
+    static public void main(String[] args) {
+        String[] testCaseName = {DatagramServiceTest.class.getName()};
+        junit.swingui.TestRunner.main(testCaseName);
+    }
+
+    // test suite from all defined tests
+    public static Test suite() {
+        TestSuite suite = new TestSuite(DatagramServiceTest.class);
+        return suite;
+    }
+}
