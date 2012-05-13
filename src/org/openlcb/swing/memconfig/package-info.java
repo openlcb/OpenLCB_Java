0295ea822e4183654c48e746b0ae93a4cc9d8d46
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2012-05-13 18:38:07 +0000

    start new package
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@2201 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

---
+++ src/org/openlcb/swing/memconfig/package-info.java
@@ -0,0 +1,13 @@
+
+/**
+Swing JPanels for doing direct manipulation via the Memory Configuration Protocol
+<p>
+This is not intended as a base for production implementations,
+but rather a convenient example of implementing
+of OpenLCB protocols.
+<p>
+The various "Pane" classes implement independent pieces that can be 
+arranged as needed. For example, a simple configuration window doesn't
+need to show the description information pane.
+*/
+package org.openlcb.swing.memconfig;
