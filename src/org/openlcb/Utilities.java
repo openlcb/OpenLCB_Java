45d3dfbe855403caf3d01791ade6175527559826
Bob Jacobsen <jacobsen@mac.com>
2012-05-17 07:17:37 +0000

    more hex string utilities
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@2232 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- src/org/openlcb/Utilities.java
+++ src/org/openlcb/Utilities.java
@@ -34,4 +34,77 @@ public class Utilities {
         String retval = new String(buff);
         return retval.substring(1);
     }
+
+    @CheckReturnValue
+    @NonNull
+    static public String toHexDotsString(int[] array) {
+        StringBuffer buff = new StringBuffer();
+        for (int i = 0; i < array.length; i++) {
+            buff.append(".");
+            buff.append(Utilities.toHexPair(array[i]));
+        }
+        String retval = new String(buff);
+        return retval.substring(1);
+    }
+
+    @CheckReturnValue
+    @NonNull
+    static public String toHexDotsString(byte[] array) {
+        StringBuffer buff = new StringBuffer();
+        for (int i = 0; i < array.length; i++) {
+            buff.append(".");
+            buff.append(Utilities.toHexPair(array[i]));
+        }
+        String retval = new String(buff);
+        return retval.substring(1);
+    }
+
+    /**
+     * Create a byte[] from a String containing hexadecimal values.
+     *
+     * @param s String of hex values, ala "01 02 0A B1 21". Either spaces or dots
+     * can be used as separators.
+     * @return byte array, with one byte for each pair.  Can be zero length,
+     *  but will not be null.
+     */
+    static public byte[] bytesFromHexString(String s) {
+        String ts = s.replace("."," ")+"  "; // ensure blanks on end to make scan easier
+        int len = 0;
+        // scan for length
+        for (int i= 0; i< s.length(); i++) {
+            if (ts.charAt(i) != ' ')  {
+                // need to process char for number. Is this a single digit?
+                if (ts.charAt(i+1) != ' ') {
+                    // 2 char value
+                    i++;
+                    len++;
+                } else {
+                    // 1 char value
+                    len++;
+                }
+            }
+        }
+        byte[] b = new byte[len];
+        // scan for content
+        int saveAt = 0;
+        for (int i= 0; i< s.length(); i++) {
+            if (ts.charAt(i) != ' ')  {
+                // need to process char for number. Is this a single digit?
+                if (ts.charAt(i+1) != ' ') {
+                    // 2 char value
+                    String v = ""+ts.charAt(i)+ts.charAt(i+1);
+                    b[saveAt] = (byte)Integer.valueOf(v,16).intValue();
+                    i++;
+                    saveAt++;
+                } else {
+                    // 1 char value
+                    String v = ""+ts.charAt(i);
+                    b[saveAt] = (byte)Integer.valueOf(v,16).intValue();
+                    saveAt++;
+                }
+            }
+        }
+        return b;
+    }
+
 }
