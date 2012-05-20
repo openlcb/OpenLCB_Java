e91970b3c5bc8ddb352a68acf1d929581bf0239b
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2012-05-20 20:26:24 +0000

    convenience methods and constants for error codes
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@2257 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- src/org/openlcb/DatagramRejectedMessage.java
+++ src/org/openlcb/DatagramRejectedMessage.java
@@ -36,4 +36,21 @@ public class DatagramRejectedMessage extends AddressedMessage {
      }
 
     public int getMTI() { return MTI_DATAGRAM_REJECTED; }
+    
+    static final int DATAGRAM_REJECTED                           = 0x000;
+    static final int DATAGRAM_REJECTED_PERMANENT_ERROR           = 0x100;
+    static final int DATAGRAM_REJECTED_INFORMATION_LOGGED        = 0x101;
+    static final int DATAGRAM_REJECTED_SOURCE_NOT_PERMITTED      = 0x102;
+    static final int DATAGRAM_REJECTED_DATAGRAMS_NOT_ACCEPTED    = 0x104;
+    static final int DATAGRAM_REJECTED_BUFFER_FULL               = 0x200;
+    static final int DATAGRAM_REJECTED_OUT_OF_ORDER              = 0x600;
+    
+    static final int DATAGRAM_REJECTED_NO_RESEND_MASK            = 0x100;
+    static final int DATAGRAM_REJECTED_RESEND_MASK               = 0x200;
+    static final int DATAGRAM_REJECTED_TRANSPORT_ERROR_MASK      = 0x400;
+    
+    
+    public boolean canResend() { 
+        return (code & DATAGRAM_REJECTED_RESEND_MASK) == DATAGRAM_REJECTED_RESEND_MASK;
+    }
 }
