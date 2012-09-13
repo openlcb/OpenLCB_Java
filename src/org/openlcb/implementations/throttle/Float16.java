844eeecde3a0983769cd3d219b146ddced733a74
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2012-09-13 04:40:57 +0000

    initial class structure
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@2722 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

---
+++ src/org/openlcb/implementations/throttle/Float16.java
@@ -0,0 +1,16 @@
+package org.openlcb.implementations.throttle;
+
+import net.jcip.annotations.Immutable;
+import net.jcip.annotations.ThreadSafe;
+import org.openlcb.*;
+
+/**
+ * Represents a 16-bit IEEE float.
+ *
+ *
+ * @author  Bob Jacobsen   Copyright 2012
+ * @version $Revision$
+ */
+public class Float16 {
+
+}
--- test/org/openlcb/implementations/throttle/ThrottleSpeedDatagramTest.java
+++ test/org/openlcb/implementations/throttle/ThrottleSpeedDatagramTest.java
@@ -1,5 +1,7 @@
 package org.openlcb.implementations.throttle;
 
+import org.openlcb.*;
+
 import junit.framework.Assert;
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -9,29 +11,26 @@ import junit.framework.TestSuite;
  * @author  Bob Jacobsen   Copyright 2012
  * @version $Revision$
  */
-public class PackageTest extends TestCase {
+public class ThrottleSpeedDatagramTest extends TestCase {
+    
     public void testStart() {
     }
     
     // from here down is testing infrastructure
     
-    public PackageTest(String s) {
+    public ThrottleSpeedDatagramTest(String s) {
         super(s);
     }
 
     // Main entry point
     static public void main(String[] args) {
-        String[] testCaseName = {PackageTest.class.getName()};
+        String[] testCaseName = {ThrottleSpeedDatagramTest.class.getName()};
         junit.swingui.TestRunner.main(testCaseName);
     }
 
     // test suite from all defined tests
     public static Test suite() {
-        TestSuite suite = new TestSuite(PackageTest.class);
-
-        suite.addTest(Float16Test.suite());
-        suite.addTest(ThrottleSpeedDatagramTest.suite());
-
+        TestSuite suite = new TestSuite(ThrottleSpeedDatagramTest.class);
         return suite;
     }
 }
