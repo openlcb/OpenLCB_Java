211f79f6017bcfabdac5b8bf61d6e12aa3be921a
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2012-09-13 06:07:00 +0000

    initial test cases
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@2723 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- src/org/openlcb/implementations/throttle/Float16.java
+++ src/org/openlcb/implementations/throttle/Float16.java
@@ -11,6 +11,63 @@ import org.openlcb.*;
  * @author  Bob Jacobsen   Copyright 2012
  * @version $Revision$
  */
+@Immutable
+@ThreadSafe
 public class Float16 {
 
+    public Float16(float f) {
+        double d = f;
+        if (d == 0.0) {
+            byte1 = 0;
+            byte2 = 0;
+            return;
+        }
+        
+        boolean sign = false;
+        
+        if (d < 0) {
+            sign = true;
+            d = -1 * d;
+        }
+        int exp = 15;
+        if (d < 1.0) {
+            while (d < 1.0) {
+                d = 2*d;
+                exp--;
+            }
+        } else {
+            while (d >= 2.0) {
+                d = d/2;
+                exp++;
+            }
+        }
+        
+        int ch =  ((int)(d*1024.))&0x3FF;
+        if ((((int)(d*1024.))&0x400) != 0x400) System.out.println("normalization failed for "+f+" with d="+d+" exp="+exp);
+        int bits = ch | (exp<<10);
+        if (sign) bits = bits | 0x8000;
+        
+        byte1 = (byte)((bits >> 8)&0xFF);
+        byte2 = (byte)(bits&0xFF);
+    }
+    
+    public Float16(int i) {
+        byte1 = (byte)((i>>8)&0xFF);
+        byte2 = (byte)(i&0xFF);
+    }
+    
+    byte byte1, byte2;
+    
+    public int getInt() { 
+        return ((byte1&0xFF)<<8)|(byte2&0xFF); 
+    }
+    
+    public float getFloat() { 
+        if (byte1 == 0 && byte2 == 0) return 0.0f;
+        int ch = (byte2&0xFF) | ((byte1&0x3)<<8) | 0x400;
+        int exp = (( ((int)byte1)&0x7C)>>2)-15;
+        int sign = ( (byte1 & 0x80) !=0 ) ? -1 : +1;
+        return (float)(((double)ch)/1024.0*((double)(1<<exp)))*sign;
+    }
+    
 }
--- test/org/openlcb/implementations/throttle/Float16Test.java
+++ test/org/openlcb/implementations/throttle/Float16Test.java
@@ -8,12 +8,71 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
+ * See http://en.wikipedia.org/wiki/Half-precision_floating-point_format
+ *     http://www.mathworks.com/matlabcentral/fileexchange/23173
+ *
+ * 0 01111 0000000000 = 1
+ * 1 10000 0000000000 = −2
+ * 0 11110 1111111111 = 65504
+ * 0 00000 0000000000 = 0
+ * 1 00000 0000000000 = −0
+ * 0 01101 0101010101 ≈ 0.33325... ≈ 1/3 
+ *
+ * 
  * @author  Bob Jacobsen   Copyright 2012
  * @version $Revision$
  */
 public class Float16Test extends TestCase {
+    Float16 f;
     
-    public void testStart() {
+    public void testZeroAsBits() {
+        f = new Float16(0.0f);
+        Assert.assertEquals("zero", 0, f.getInt());
+    }
+    
+    public void testOneAsBits() {
+        f = new Float16(1.0f);
+        Assert.assertEquals("one", 0x3C00, f.getInt());
+    }
+    
+    public void testTwoAsBits() {
+        f = new Float16(2.0f);
+        Assert.assertEquals("two", 0x4000, f.getInt());
+    }
+    
+    public void testNegTwoAsBits() {
+        f = new Float16(-2.0f);
+        Assert.assertEquals("-two", 0xC000, f.getInt());
+    }
+    
+    public void testMaxAsBits() {
+        f = new Float16(65504.0f);
+        Assert.assertEquals("65504", 0x7BFF, f.getInt());
+    }
+    
+    public void testZeroAsFloat() {
+        f = new Float16(0);
+        Assert.assertEquals("zero", 0.0f, f.getFloat());
+    }
+    
+    public void testOneAsFloat() {
+        f = new Float16(0x3C00);
+        Assert.assertEquals("one", 1.0f, f.getFloat());
+    }
+    
+    public void testTwoAsFloat() {
+        f = new Float16(0x4000);
+        Assert.assertEquals("two", 2.0f, f.getFloat());
+    }
+    
+    public void testNegTwoAsFloat() {
+        f = new Float16(0xC000);
+        Assert.assertEquals("-two", -2.0f, f.getFloat());
+    }
+    
+    public void testMaxAsFloat() {
+        f = new Float16(0x7BFF);
+        Assert.assertEquals("65504", 65504.0f, f.getFloat());
     }
     
     // from here down is testing infrastructure
