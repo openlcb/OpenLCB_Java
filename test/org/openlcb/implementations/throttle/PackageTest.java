5c5e70ceba0866b824307d02e0cfe2adaf4d7621
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2012-09-13 04:29:22 +0000

    package infrastructure for throttle implementation
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@2721 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

---
+++ test/org/openlcb/implementations/throttle/PackageTest.java
@@ -0,0 +1,34 @@
+package org.openlcb.implementations.throttle;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * @author  Bob Jacobsen   Copyright 2012
+ * @version $Revision$
+ */
+public class PackageTest extends TestCase {
+    public void testStart() {
+    }
+    
+    // from here down is testing infrastructure
+    
+    public PackageTest(String s) {
+        super(s);
+    }
+
+    // Main entry point
+    static public void main(String[] args) {
+        String[] testCaseName = {PackageTest.class.getName()};
+        junit.swingui.TestRunner.main(testCaseName);
+    }
+
+    // test suite from all defined tests
+    public static Test suite() {
+        TestSuite suite = new TestSuite(PackageTest.class);
+
+        return suite;
+    }
+}
