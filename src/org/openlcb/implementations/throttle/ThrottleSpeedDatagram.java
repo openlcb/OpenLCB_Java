212de5ed5aa0693be3a9f34e5b6ca662cea0315e
Bob Jacobsen <jacobsen@mac.com>
2012-09-13 04:40:57 +0000

    initial class structure
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@2722 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

---
+++ src/org/openlcb/implementations/throttle/ThrottleSpeedDatagram.java
@@ -0,0 +1,23 @@
+package org.openlcb.implementations.throttle;
+
+import net.jcip.annotations.Immutable;
+import net.jcip.annotations.ThreadSafe;
+import org.openlcb.*;
+
+/**
+ * Speed control datagram from throttle to command station.
+ * 
+ * Meant to shield the using code from all the details of that
+ * process via some primitives:
+ * <ul>
+ * <li>
+ * <li>
+ * </ul>
+ *
+ *
+ * @author  Bob Jacobsen   Copyright 2012
+ * @version $Revision$
+ */
+public class ThrottleSpeedDatagram {
+
+}
