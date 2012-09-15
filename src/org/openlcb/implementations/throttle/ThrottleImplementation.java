bf60ed7b8cbdb66efcda5e9a8a2f11fc12929a7f
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2012-09-15 03:10:15 +0000

    add function write
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@2732 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- src/org/openlcb/implementations/throttle/ThrottleImplementation.java
+++ src/org/openlcb/implementations/throttle/ThrottleImplementation.java
@@ -43,10 +43,19 @@ public class ThrottleImplementation {
     
     /**
      * @param speed Desired speed in scale meters/second. By convention, 100 m/sec is full speed
-     * for DCC locomotives.
+     * for DCC locomotives. Negative is reverse motion.
      */
     public void setSpeed(double speed) {
         ThrottleSpeedDatagram tsd = new ThrottleSpeedDatagram(speed);
+        service.sendData(dest, tsd.getData());
+    }
+    
+    /**
+     * @param number 0 - ?? number of DCC function (0-28 space)
+     * @param value 0 for off, non-zero for on
+     */
+    public void setFunction(int number, int value) {
+        ThrottleFunctionDatagram tsd = new ThrottleFunctionDatagram(number, value);
         service.sendData(dest, tsd.getData());
     }
     
