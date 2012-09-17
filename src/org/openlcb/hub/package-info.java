a55146fcb8f679856fa3a837cf99d1962058682d
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2012-09-17 17:20:54 +0000

    inital version, working in self-contained form
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@2741 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

---
+++ src/org/openlcb/hub/package-info.java
@@ -0,0 +1,8 @@
+
+/**
+A Java implementation of a TCP/IP connected node hub for OpenLCB.
+<p>
+This is not intended as a base for production implementations,
+but rather a convenient demonstration tool.
+*/
+package org.openlcb.hub;
