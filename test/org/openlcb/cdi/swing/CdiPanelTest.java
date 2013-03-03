bb2c5286b8ed8e614ea53b8b3fd01ae9fc7ca633
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2013-03-03 20:54:03 +0000

    check CDI for locomotive
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@3035 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- test/org/openlcb/cdi/swing/CdiPanelTest.java
+++ test/org/openlcb/cdi/swing/CdiPanelTest.java
@@ -8,12 +8,13 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import javax.swing.*;
+
+import org.jdom.*;
+import org.jdom.input.SAXBuilder;
 
 /**
  * @author  Bob Jacobsen   Copyright 2012
@@ -33,29 +34,74 @@ public class CdiPanelTest extends TestCase {
         CdiPanel m = new CdiPanel();
                 
         m.initComponents(new CdiPanel.ReadWriteAccess(){
-            @Override
-            public void doWrite(long address, int space, byte[] data) {
-                    System.out.println(data.length);
-                    System.out.println("write "+address+" "+space+": "+org.openlcb.Utilities.toHexDotsString(data));
+                @Override
+                public void doWrite(long address, int space, byte[] data) {
+                        System.out.println(data.length);
+                        System.out.println("write "+address+" "+space+": "+org.openlcb.Utilities.toHexDotsString(data));
+                    }
+                @Override
+                public void doRead(long address, int space, int length, CdiPanel.ReadReturn handler) {
+                        handler.returnData(new byte[]{1,2,3,4,5,6,7,8});
+                        System.out.println("read "+address+" "+space);
+                    }            
+            },
+                new CdiPanel.GuiItemFactory() {
+                    public JButton handleReadButton(JButton button) {
+                        //System.out.println("process button");
+                        button.setBorder(BorderFactory.createLineBorder(java.awt.Color.yellow));
+                        return button;
                 }
-            @Override
-            public void doRead(long address, int space, int length, CdiPanel.ReadReturn handler) {
-                    handler.returnData(new byte[]{1,2,3,4,5,6,7,8});
-                    System.out.println("read "+address+" "+space);
-                }            
-        },
-        new CdiPanel.GuiItemFactory() {
-            public JButton handleReadButton(JButton button) {
-                //System.out.println("process button");
-                button.setBorder(BorderFactory.createLineBorder(java.awt.Color.yellow));
-                return button;
-        }
-    }
-);
+            }
+        );
         m.loadCDI(
             new org.openlcb.cdi.jdom.JdomCdiRep(
                 org.openlcb.cdi.jdom.SampleFactory.getBasicSample()
             )
+        );
+        
+        f.add( m );
+
+        // show
+        f.pack();
+        f.setVisible(true);        
+    }
+    
+    public void testLocoCdiDisplay() {
+        JFrame f = new JFrame();
+        f.setTitle("Locomotive CDI Demonstration");
+        CdiPanel m = new CdiPanel();
+                
+        m.initComponents(new CdiPanel.ReadWriteAccess(){
+                @Override
+                public void doWrite(long address, int space, byte[] data) {
+                        System.out.println(data.length);
+                        System.out.println("write "+address+" "+space+": "+org.openlcb.Utilities.toHexDotsString(data));
+                    }
+                @Override
+                public void doRead(long address, int space, int length, CdiPanel.ReadReturn handler) {
+                        handler.returnData(new byte[]{1,2,3,4,5,6,7,8});
+                        System.out.println("read "+address+" "+space);
+                    }            
+            },
+                new CdiPanel.GuiItemFactory() {
+                    public JButton handleReadButton(JButton button) {
+                        //System.out.println("process button");
+                        button.setBorder(BorderFactory.createLineBorder(java.awt.Color.yellow));
+                        return button;
+                }
+            }
+        );
+        
+        // find file & load file
+        Element root = null;
+        try {
+            SAXBuilder builder = new SAXBuilder("org.apache.xerces.parsers.SAXParser", false);  // argument controls validation
+            Document doc = builder.build(new BufferedInputStream(new FileInputStream(new File("0nmraCDI.xml"))));
+            root = doc.getRootElement();
+        } catch (Exception e) { System.out.println("While reading file: "+e);}
+        
+        m.loadCDI(
+            new org.openlcb.cdi.jdom.JdomCdiRep(root)
         );
         
         f.add( m );
