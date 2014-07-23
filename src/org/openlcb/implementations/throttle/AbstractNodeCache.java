bec11a409a8f369d7ad6fe451b61173c1207e8e9
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2014-07-23 10:02:25 +0000

    improve bean structure, make more generic (not yet complete)
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@3597 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- src/org/openlcb/implementations/throttle/AbstractNodeCache.java
+++ src/org/openlcb/implementations/throttle/AbstractNodeCache.java
@@ -4,22 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 import org.openlcb.Connection;
 import org.openlcb.EventID;
+import org.openlcb.NodeID;
 import org.openlcb.MessageDecoder;
 import org.openlcb.ProducerConsumerEventReportMessage;
 
 /**
+ * Maintains a cache of nodes seen to emit a particular EventID.
+ *
+ * @TODO:  Type of node needs to be made generic T
+ * 
+ * @TODO: make sure name and semantics of contained property are correct for a Bean, e.g.
+ * it has the right name and related methods are present.
+ *     http://docs.oracle.com/javase/tutorial/javabeans/writing/properties.html
+ * How are indexed properties handled in Events?
  *
  * @author Bob Jacobsen      Copyright 2012
  */
-public class AbstractNodeCache extends MessageDecoder {
+public abstract class AbstractNodeCache<T> extends MessageDecoder {
 
     public AbstractNodeCache(EventID indicator) {
         this.indicator = indicator;
     }
-    ArrayList<TrainNode> list = new ArrayList<TrainNode>();
+    ArrayList<T> list = new ArrayList<T>();
     EventID indicator;
+    
+    // for indexed Bean form
+    public T[] getCache() {
+        return (T[])list.toArray();
+    }
+    public T getCache(int index) throws ArrayIndexOutOfBoundsException {
+        return list.get(index);
+    }
+    
 
-    public List<TrainNode> getList() {
+    public List<T> getList() {
         return list;
     }
 
@@ -27,11 +45,16 @@ public class AbstractNodeCache extends MessageDecoder {
         EventID evt = msg.getEventID();
         if (evt.equals(indicator)) {
             // get source as Node ID for train node
-            TrainNode tn = new TrainNode(msg.getSourceNodeID());
-            list.add(tn);
-            firePropertyChange("TrainNode", null, tn);
+            T node = newObject(msg.getSourceNodeID());
+            list.add(node);
+            firePropertyChange("cache", null, node);
         }
     }
+    
+    /**
+     * Implement this for specific type to be created in this cache - inelegant!
+     */
+    protected abstract T newObject(NodeID id);
     
     java.beans.PropertyChangeSupport pcs = new java.beans.PropertyChangeSupport(this);
     public synchronized void addPropertyChangeListener(java.beans.PropertyChangeListener l) {
