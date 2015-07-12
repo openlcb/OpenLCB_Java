c929cbdb26b9d8bf1433cf9a99327653648b497f
Bob Jacobsen <jacobsen@mac.com>
2015-07-12 17:12:41 +0000

    generalize the CDI test for demo/development purposes
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@3891 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

---
+++ test/org/openlcb/cdi/swing/CdiPanelDemo.java
@@ -0,0 +1,98 @@
+package org.openlcb.cdi.swing;
+
+import javax.swing.JFrame;
+import org.openlcb.*;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.swing.*;
+
+import org.jdom2.*;
+import org.jdom2.input.SAXBuilder;
+
+/**
+ * Provide some demo & development tools for CDI
+ *
+ * @author  Bob Jacobsen   Copyright 2012, 2015
+ * @version $Revision: 2175 $
+ */
+public class CdiPanelDemo extends TestCase {
+        
+    public CdiPanelDemo() {
+    }
+
+    public void displayFile() {
+        JFrame f = new JFrame();
+        CdiPanel m = new CdiPanel();
+                
+        m.initComponents(new CdiPanel.ReadWriteAccess(){
+                @Override
+                public void doWrite(long address, int space, byte[] data) {
+                        System.out.println(data.length);
+                        System.out.println("write "+address+" "+space+": "+org.openlcb.Utilities.toHexDotsString(data));
+                    }
+                @Override
+                public void doRead(long address, int space, int length, CdiPanel.ReadReturn handler) {
+                        handler.returnData(new byte[]{1,2,3,4,5,6,7,8});
+                        System.out.println("read "+address+" "+space);
+                    }            
+            },
+                new CdiPanel.GuiItemFactory() {
+                    public JButton handleReadButton(JButton button) {
+                        //System.out.println("process button");
+                        button.setBorder(BorderFactory.createLineBorder(java.awt.Color.yellow));
+                        return button;
+                }
+            }
+        );
+        
+        // find file & load file
+        fci.setDialogTitle("Find desired script file");
+        fci.rescanCurrentDirectory();
+
+        int retVal = fci.showOpenDialog(null);
+        // handle selection or cancel
+        if (retVal != JFileChooser.APPROVE_OPTION) {
+            File file = fci.getSelectedFile();
+            // Run the script from it's filename
+            System.out.println("No file selected");
+        }
+
+        f.setTitle(fci.getSelectedFile().getName());
+
+        Element root = null;
+        try {
+            SAXBuilder builder = new SAXBuilder("org.apache.xerces.parsers.SAXParser", false);  // argument controls validation
+            Document doc = builder.build(new BufferedInputStream(new FileInputStream(fci.getSelectedFile())));
+            root = doc.getRootElement();
+        } catch (Exception e) { System.out.println("While reading file: "+e);}
+        
+        m.loadCDI(
+            new org.openlcb.cdi.jdom.JdomCdiRep(root)
+        );
+        
+        f.add( m );
+
+        // show
+        f.pack();
+        f.setVisible(true);        
+    }
+    
+    /**
+     * We always use the same file chooser in this class, so that the user's
+     * last-accessed directory remains available.
+     */
+    static JFileChooser fci = new JFileChooser();
+
+    // Main entry point
+    static public void main(String[] args) {
+        CdiPanelDemo d = new CdiPanelDemo();
+        d.displayFile();
+    }
+}
