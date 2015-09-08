4065e7a7daffa97abb89763b35fd0b9e66c07375
bracz <bracz@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2015-09-08 20:30:17 +0000

    Adds a regression test for the sign extension bugfix https://sourceforge.net/p/openlcb/svn/3920/
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@3923 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- test/org/openlcb/can/MessageBuilderTest.java
+++ test/org/openlcb/can/MessageBuilderTest.java
@@ -410,7 +410,29 @@ public class MessageBuilderTest extends TestCase {
         Assert.assertEquals("source", source, msg.getSourceNodeID());
         Assert.assertEquals("destination", high, ((AddressedMessage)msg).getDestNodeID());
     }
+
+    public void testAddressedMessageAliasExtraction() {
     
+        NodeID high = new NodeID(new byte[]{11,12,13,14,15,16});
+        map.insert(0x0FFF, high);
+        
+        OpenLcbCanFrame frame = new OpenLcbCanFrame(0x123);
+        frame.setHeader(0x19A28123);
+        frame.setData(new byte[]{(byte)0x0F, (byte)0xFF});
+        
+        MessageBuilder b = new MessageBuilder(map);
+        
+        List<Message> list = b.processFrame(frame);
+        
+        Assert.assertEquals("count", 1, list.size()); 
+        Message msg = list.get(0);
+        
+        Assert.assertTrue(msg instanceof AddressedMessage);        
+
+        Assert.assertEquals("source", source, msg.getSourceNodeID());
+        Assert.assertEquals("destination", high, ((AddressedMessage)msg).getDestNodeID());
+    }
+
     // from here down is testing infrastructure
     
     public MessageBuilderTest(String s) {
