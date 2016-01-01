108a85f7e6082d5e960ce8f5d41d0ef4ea0ecf47
bracz <bracz@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2016-01-01 20:07:48 +0000

    Adds a set of utility classes that allow different components to agree upon a shared value, where updates can come from either ofthese components.
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@3967 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

---
+++ src/org/openlcb/implementations/VersionedValueListener.java
@@ -0,0 +1,47 @@
+package org.openlcb.implementations;
+
+import org.openlcb.implementations.VersionedValue;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+/**
+ * Created by bracz on 12/30/15.
+ */
+public abstract class VersionedValueListener<T> implements PropertyChangeListener {
+    int ownerVersion;
+    VersionedValue<T> parent;
+
+    public VersionedValueListener(VersionedValue<T> parent) {
+        this.parent = parent;
+        parent.addPropertyChangeListener(this);
+        ownerVersion = parent.getVersion();
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
+        int pVersion = parent.getVersion();
+        boolean isUpdated = false;
+        synchronized (this) {
+            if (pVersion > ownerVersion) {
+                ownerVersion = pVersion;
+                isUpdated = true;
+            }
+        }
+        if (isUpdated) {
+            update(parent.getLatestData());
+        }
+    }
+
+    public void setFromOwner(T t) {
+        int newVersion;
+        synchronized (this) {
+            newVersion = parent.getNewVersion();
+            ownerVersion = newVersion;
+        }
+        parent.set(newVersion, t);
+    }
+
+    /// Called when the backend has to update its version.
+    public abstract void update(T t);
+}
