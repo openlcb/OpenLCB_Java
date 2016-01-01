566a016ff8ebdd0bc90d1fe7a9f2b2368bf11445
Balazs Racz <balazs.racz@gmail.com>
2016-01-01 20:05:02 +0000

    Reformat.
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@3958 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- src/org/openlcb/implementations/throttle/ThrottleImplementation.java
+++ src/org/openlcb/implementations/throttle/ThrottleImplementation.java
@@ -1,6 +1,7 @@
 package org.openlcb.implementations.throttle;
 
-import org.openlcb.*;
+import org.openlcb.MimicNodeStore;
+import org.openlcb.NodeID;
 import org.openlcb.implementations.DatagramService;
 
 /**
@@ -8,47 +9,47 @@ import org.openlcb.implementations.DatagramService;
  * that uses the Motive Power protocol(s)
  * to interface with e.g. a train.
  *
- * @author  Bob Jacobsen   Copyright 2012
+ * @author Bob Jacobsen   Copyright 2012
  * @version $Revision$
  */
 public class ThrottleImplementation {
+    MimicNodeStore store;
+    DatagramService service;
+    NodeID dest;
 
-    public ThrottleImplementation(NodeID node, 
+    public ThrottleImplementation(NodeID node,
                                   MimicNodeStore store, DatagramService service) {
         this.dest = node;
         this.store = store;
         this.service = service;
     }
 
-    public ThrottleImplementation(int dccAddress, boolean dccLongAddress, 
+    public ThrottleImplementation(int dccAddress, boolean dccLongAddress,
                                   MimicNodeStore store, DatagramService service) {
         this(createNodeIdFromDcc(dccAddress, dccLongAddress), store, service);
     }
 
-    MimicNodeStore store;
-    DatagramService service;
-    NodeID dest;
-    
     static NodeID createNodeIdFromDcc(int dccAddress, boolean dccLongAddress) {
         if (dccLongAddress)
-            return new NodeID(new byte[]{6,1,0,0,(byte)(((dccAddress>>8) & 0xFF) | 0xC0), (byte)(dccAddress & 0xFF)});
+            return new NodeID(new byte[]{6, 1, 0, 0, (byte) (((dccAddress >> 8) & 0xFF) | 0xC0),
+                    (byte) (dccAddress & 0xFF)});
         else
-            return new NodeID(new byte[]{6,1,0,0,0, (byte)(dccAddress & 0xFF)});
+            return new NodeID(new byte[]{6, 1, 0, 0, 0, (byte) (dccAddress & 0xFF)});
     }
-    
+
     public void start() {
         store.findNode(dest);
     }
-    
+
     /**
      * @param speed Desired speed in scale meters/second. By convention, 100 m/sec is full speed
-     * for DCC locomotives.
+     *              for DCC locomotives.
      */
     public void setSpeed(double speed, boolean forward) {
         ThrottleSpeedDatagram tsd = new ThrottleSpeedDatagram(speed, forward);
         service.sendData(dest, tsd.getData());
     }
-    
+
     /**
      * Set emergency stop; unset by any following setSpeed command
      */
@@ -56,14 +57,14 @@ public class ThrottleImplementation {
         ThrottleSpeedDatagram tsd = new ThrottleSpeedDatagram();
         service.sendData(dest, tsd.getData());
     }
-    
+
     /**
      * @param number 0 - ?? number of DCC function (0-28 space)
-     * @param value 0 for off, non-zero for on
+     * @param value  0 for off, non-zero for on
      */
     public void setFunction(int number, int value) {
         ThrottleFunctionDatagram tsd = new ThrottleFunctionDatagram(number, value);
         service.sendData(dest, tsd.getData());
     }
-    
+
 }
