c0f0977e7f7c981bad1e8975d56ffcb12b1f00d7
Balazs Racz <balazs.racz@gmail.com>
2016-01-01 20:06:55 +0000

    Registers the TrainNodeCache upon instantiation for getting network messages. Instantiates PIP and SNIP requests on the train nodes as they show up.
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@3964 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- src/org/openlcb/implementations/throttle/TrainNodeCache.java
+++ src/org/openlcb/implementations/throttle/TrainNodeCache.java
@@ -1,24 +1,43 @@
 package org.openlcb.implementations.throttle;
 
-import org.openlcb.*;
+import org.openlcb.CommonIdentifiers;
+import org.openlcb.Connection;
+import org.openlcb.IdentifyProducersMessage;
+import org.openlcb.MimicNodeStore;
+import org.openlcb.NodeID;
+import org.openlcb.OlcbInterface;
 
 /**
  * Maintain a cache of Train objects on OpenLCB network
  *
- *
- * @author  Bob Jacobsen   Copyright 2012
+ * @author Bob Jacobsen   Copyright 2012
  * @version $Revision$
  */
 public class TrainNodeCache extends AbstractNodeCache<RemoteTrainNode> {
 
     private final OlcbInterface iface;
 
-    public TrainNodeCache(OlcbInterface iface) {
+    public TrainNodeCache(OlcbInterface _iface) {
         super(CommonIdentifiers.IS_TRAIN);
-        this.iface = iface;
+        iface = _iface;
+        iface.registerMessageListener(this);
+        // Sends a query to all train nodes on the network.
+        iface.getOutputConnection().registerStartNotification(new ConnectionListener() {
+            @Override
+            public void connectionActive(Connection c) {
+                c.put(new IdentifyProducersMessage(iface.getNodeId(), CommonIdentifiers.IS_TRAIN)
+                        , TrainNodeCache.this);
+            }
+        });
     }
-    
+
     protected RemoteTrainNode newObject(NodeID id) {
+        MimicNodeStore.NodeMemo memo = iface.getNodeStore().findNode(id);
+        if (memo != null) {
+            // Instantiates the fetching of SNIP and PIP data.
+            memo.getSimpleNodeIdent();
+            memo.getProtocolIdentification();
+        }
         return new RemoteTrainNode(id);
     }
 
