b97415381544ff3afa22b535a230405805871ca3
Balazs Racz <balazs.racz@gmail.com>
2016-01-01 20:07:48 +0000

    Adds a set of utility classes that allow different components to agree upon a shared value, where updates can come from either ofthese components.
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/java@3967 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

---
+++ src/org/openlcb/implementations/VersionOutOfDateException.java
@@ -0,0 +1,7 @@
+package org.openlcb.implementations;
+
+/**
+ * Created by bracz on 12/30/15.
+ */
+public class VersionOutOfDateException extends Exception {
+}
---
+++ src/org/openlcb/implementations/VersionedValue.java
@@ -0,0 +1,68 @@
+package org.openlcb.implementations;
+
+/**
+ * Created by bracz on 12/30/15.
+ */
+public class VersionedValue<T> {
+    T data;
+    int version;
+    int nextVersion;
+    java.beans.PropertyChangeSupport pcs = new java.beans.PropertyChangeSupport(this);
+
+    public void VersionedValue(T t) {
+        version = 1;
+        nextVersion = 2;
+        data = t;
+    }
+
+    public int getNewVersion() {
+        int newVersion;
+        synchronized (this) {
+            newVersion = ++nextVersion;
+        }
+        return newVersion;
+    }
+
+    public boolean set(int atVersion, T t) {
+        T old;
+        synchronized (this) {
+            if (atVersion <= version) return false;
+            version = atVersion;
+            if (nextVersion <= atVersion) {
+                nextVersion = atVersion + 1;
+            }
+            old = data;
+            data = t;
+        }
+        firePropertyChange("updated", old, t);
+        return true;
+    }
+
+    public T getLatestData() {
+        return data;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public synchronized T getDataForVersion(int expectedVersion) throws VersionOutOfDateException {
+        if (version != expectedVersion) {
+            throw new VersionOutOfDateException();
+        }
+        return data;
+    }
+
+    public synchronized void addPropertyChangeListener(java.beans.PropertyChangeListener l) {
+        pcs.addPropertyChangeListener(l);
+    }
+
+    public synchronized void removePropertyChangeListener(java.beans.PropertyChangeListener l) {
+        pcs.removePropertyChangeListener(l);
+    }
+
+    protected void firePropertyChange(String p, Object old, Object n) {
+        pcs.firePropertyChange(p, old, n);
+    }
+
+}
