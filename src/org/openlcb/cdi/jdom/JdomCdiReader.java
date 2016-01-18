62b383f29b562ad783a75f8dee22a15bdf225326
Balazs Racz <balazs.racz@gmail.com>
2016-01-18 22:36:19 +0100

    Adds more try/catch phrases around xerces-specific features of the XML parsing.

--- src/org/openlcb/cdi/jdom/JdomCdiReader.java
+++ src/org/openlcb/cdi/jdom/JdomCdiReader.java
@@ -14,21 +14,33 @@ import org.jdom2.input.SAXBuilder;
  */
 public class JdomCdiReader {
 
-    public Element getHeadFromReader(Reader rdr) throws Exception {
-        SAXBuilder builder = new SAXBuilder("org.apache.xerces.parsers.SAXParser",false);  // argument controls validation
-        
-        //builder.setEntityResolver(new jmri.util.JmriLocalEntityResolver());
-        builder.setFeature("http://apache.org/xml/features/xinclude", true);
-        builder.setFeature("http://apache.org/xml/features/xinclude/fixup-base-uris", false);
-        
+    public static Element getHeadFromReader(Reader rdr) throws Exception {
+        String parserName = org.apache.xerces.parsers.SAXParser.class.getName();
+        SAXBuilder builder;
+
+        try {
+            //"org.apache.xerces.parsers.SAXParser";
+            builder = new SAXBuilder(parserName,false);  // argument
+            // controls validation
+        } catch (Exception e) {
+            builder = new SAXBuilder(false);
+        }
+
+        try {
+            //builder.setEntityResolver(new jmri.util.JmriLocalEntityResolver());
+            builder.setFeature("http://apache.org/xml/features/xinclude", true);
+            builder.setFeature("http://apache.org/xml/features/xinclude/fixup-base-uris", false);
+        } catch (Exception e) {
+            System.err.println("Could not set xinclude feature: "+e);
+        }
+
         // for schema validation. Not needed for DTDs, so continue if not found now
         try {
-            builder.setFeature("http://apache.org/xml/features/validation/schema", false);
-            builder.setFeature("http://apache.org/xml/features/validation/schema-full-checking", false);
-
             // parse namespaces, including the noNamespaceSchema
             builder.setFeature("http://xml.org/sax/features/namespaces", true);
 
+            builder.setFeature("http://apache.org/xml/features/validation/schema", false);
+            builder.setFeature("http://apache.org/xml/features/validation/schema-full-checking", false);
         } catch (Exception e) {
             System.err.println("Could not set schema validation feature: "+e);
         }
@@ -39,7 +51,7 @@ public class JdomCdiReader {
             return doc.getRootElement();
         } catch (JDOMException e) {
             System.err.println("Could not create Document: "+e);
-            throw new Exception(e);
+            throw e;
         } catch (IOException e) {
             System.err.println("Could not create Document: "+e);
             throw new Exception(e);
