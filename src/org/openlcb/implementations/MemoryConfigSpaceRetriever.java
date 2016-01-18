1076ae16e1efcc3363c71e9b675a630b726d8073
Balazs Racz <balazs.racz@gmail.com>
2016-01-18 22:31:14 +0100

    Adds a generic class that can download an entire Memory Config space, for example a CDI or FDI xml file.

---
+++ src/org/openlcb/implementations/MemoryConfigSpaceRetriever.java
@@ -0,0 +1,64 @@
+package org.openlcb.implementations;
+
+import org.openlcb.NodeID;
+import org.openlcb.OlcbInterface;
+
+/**
+ * Downloads the entire contents of a Memory Config space. Useful for CDI-style situations.
+ * <p/>
+ * Created by bracz on 1/17/16.
+ */
+public class MemoryConfigSpaceRetriever {
+    final static int LENGTH = 64;
+
+    final NodeID node;
+    final MemoryConfigurationService service;
+    final int space;
+    final Callback cb;
+    long nextAddress = 0;
+    StringBuffer buf;
+
+    public MemoryConfigSpaceRetriever(NodeID node, OlcbInterface iface, int space, Callback cb) {
+        this.node = node;
+        this.service = iface.getMemoryConfigurationService();
+        this.space = space;
+        this.cb = cb;
+        nextAddress = 0;
+        buf = new StringBuffer();
+        nextRequest();
+    }
+
+    void nextRequest() {
+        MemoryConfigurationService.McsReadMemo memo =
+                new MemoryConfigurationService.McsReadMemo(node, space, nextAddress, LENGTH) {
+                    public void handleReadData(NodeID dest, int space, long address, byte[] data) {
+                        // handle return data, checking for null in string or zero-length reply
+                        if (data.length == 0) {
+                            done();
+                            return;  // don't do next request
+                        }
+                        for (int i = 0; i < data.length; i++) {
+                            if (data[i] == 0) {
+                                done();
+                                return;  // don't do next request
+                            }
+                            buf.append((char) data[i]);
+                        }
+                        // repeat if not done
+                        nextAddress = nextAddress + LENGTH;
+                        nextRequest();
+                    }
+                };
+        service.request(memo);
+    }
+
+    private void done() {
+        cb.onSuccess(buf.toString());
+    }
+
+    public interface Callback {
+        void onSuccess(String contents);
+
+        void onFailure(int code);
+    }
+}
