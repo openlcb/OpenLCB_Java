a5729929caaa53c5ba89cfabe08e29927d69125c
Balazs Racz <balazs.racz@gmail.com>
2016-04-09 16:41:03 +0200

    Factors out common helpers from the command executable to a utility class.

--- src/org/openlcb/cdi/cmd/SaveConfig.java
+++ src/org/openlcb/cdi/cmd/SaveConfig.java
@@ -1,13 +1,10 @@
 package org.openlcb.cdi.cmd;
 
 import org.openlcb.NodeID;
-import org.openlcb.PropertyListenerSupport;
 import org.openlcb.Utilities;
 import org.openlcb.can.impl.OlcbConnection;
 import org.openlcb.cdi.impl.ConfigRepresentation;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -20,124 +17,11 @@ import java.nio.file.Paths;
 public class SaveConfig {
 
 
-    static void waitForPropertyChange(PropertyListenerSupport tgt, final String propertyName) {
-        final Object o = new Object();
-        PropertyChangeListener l = new PropertyChangeListener() {
-            @Override
-            public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
-                if (propertyChangeEvent.getPropertyName().equals(propertyName)) {
-                    synchronized(o) {
-                        o.notify();
-                    }
-                }
-            }
-        };
-        tgt.addPropertyChangeListener(l);
-        try {
-            synchronized(o) {
-                o.wait();
-            }
-        } catch (InterruptedException e) {
-            System.err.println("Interrupted while waiting for property notification " +
-                    propertyName + " on object of class " + tgt.getClass().getName());
-        }
-        tgt.removePropertyChangeListener(l);
-    }
-
-    static public OlcbConnection connect(final NodeID localNode, final String host, final int
-            port) {
-        class Success {
-            private boolean ok = false;
-
-            public synchronized void set(boolean isOk) {
-                ok = isOk;
-                this.notify();
-            }
-
-            public synchronized boolean get() {
-                try {
-                    this.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                return ok;
-            }
-        }
-        final Success s = new Success();
-
-        OlcbConnection.ConnectionListener l = new OlcbConnection
-                .ConnectionListener() {
-            @Override
-            public void onConnect() {
-                System.out.println("Connected to " + host + ":" + port);
-                s.set(true);
-            }
-
-            @Override
-            public void onDisconnect() {
-                System.out.println("Disconnected.");
-                s.set(false);
-            }
-
-            @Override
-            public void onStatusChange(String status) {
-                System.out.println(status);
-            }
-
-            @Override
-            public void onConnectionPending() {
-                System.out.println("Connecting.");
-            }
-        };
-
-        OlcbConnection connection = new OlcbConnection(localNode, host, port, l);
-        if (s.get()) {
-            return connection;
-        } else {
-            System.exit(1);
-            return null;
-        }
-    }
-
-    static public String escapeString(String myString) {
-        StringBuilder newString = new StringBuilder(myString.length() + 5);
-        for (int offset = 0; offset < myString.length();)
-        {
-            int codePoint = myString.codePointAt(offset);
-            offset += Character.charCount(codePoint);
-            boolean mustEscape = false;
-            if (codePoint == "=".codePointAt(0)) {
-                mustEscape = true;
-            }
-            // Replace invisible control characters and unused code points
-            switch (Character.getType(codePoint))
-            {
-                case Character.CONTROL:     // \p{Cc}
-                case Character.FORMAT:      // \p{Cf}
-                case Character.PRIVATE_USE: // \p{Co}
-                case Character.SURROGATE:   // \p{Cs}
-                case Character.UNASSIGNED:  // \p{Cn}
-                    newString.append("\\x");
-                    newString.append(String.format("%04x", codePoint));
-                    break;
-                default:
-                    if (mustEscape) {
-                        newString.append("\\x");
-                        newString.append(String.format("%04x", codePoint));
-                    } else {
-                        newString.append(Character.toChars(codePoint));
-                    }
-                    break;
-            }
-        }
-        return newString.toString();
-    }
-
     static public void writeEntry(BufferedWriter outFile, String key, String value) {
         try {
-            outFile.write(escapeString(key));
+            outFile.write(Util.escapeString(key));
             outFile.write('=');
-            outFile.write(escapeString(value));
+            outFile.write(Util.escapeString(value));
             outFile.write('\n');
         } catch (IOException e1) {
             e1.printStackTrace();
@@ -158,12 +42,12 @@ public class SaveConfig {
         final NodeID remoteNode = new NodeID(args[3]);
         final String dstFile = args[4];
 
-        final OlcbConnection connection = connect(localNode, host, port);
+        final OlcbConnection connection = Util.connect(localNode, host, port);
         System.out.println("Fetching CDI.");
         ConfigRepresentation repr = connection.getConfigForNode(remoteNode);
-        waitForPropertyChange(repr, ConfigRepresentation.UPDATE_REP);
+        Util.waitForPropertyChange(repr, ConfigRepresentation.UPDATE_REP);
         System.out.println("CDI fetch done. Waiting for caches.");
-        waitForPropertyChange(repr, ConfigRepresentation.UPDATE_CACHE_COMPLETE);
+        Util.waitForPropertyChange(repr, ConfigRepresentation.UPDATE_CACHE_COMPLETE);
         System.out.println("Caches complete.");
         BufferedWriter outFile = null;
 
