879be6b61bf0bd6910b49a9965e07b040d6650f9
Randall Wood <randall.h.wood@alexandriasoftware.com>
2017-02-22 07:49:26 -0500

    Fix a Javadoc warning flagged by Travis CI

--- src/org/openlcb/Interaction.java
+++ src/org/openlcb/Interaction.java
@@ -3,21 +3,24 @@ package org.openlcb;
 /**
  * Created by bracz on 12/7/16.
  */
-
 public abstract class Interaction {
+
     /**
-     * Timeout after each sendRequest for the interaction to be completed before the onTimeout is
-     * called.
+     * Timeout after each sendRequest for the interaction to be completed before
+     * the onTimeout is called.
      */
     int deadlineMsec = 700;
 
     /**
-     * Set to true by the system when a cancel/complete call arrives for this interaction.
+     * Set to true by the system when a cancel/complete call arrives for this
+     * interaction.
      */
     boolean isComplete = false;
 
     /**
      * Called by the system when it is time to send this interaction.
+     *
+     * @param downstream the connection over which to send the request
      */
     abstract void sendRequest(Connection downstream);
 
@@ -27,7 +30,8 @@ public abstract class Interaction {
     abstract NodeID dstNode();
 
     /**
-     * Called by the system if there is no answer from the destination node within the timeout.
+     * Called by the system if there is no answer from the destination node
+     * within the timeout.
      */
     abstract void onTimeout();
 }
