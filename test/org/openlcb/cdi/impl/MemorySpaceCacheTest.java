eab73c1fd8a48cb0c8f962eda7c085bbc121c7f3
Paul Bender <paul.bender@acm.org>
2018-03-16 23:18:44 -0400

    reorganize test

--- test/org/openlcb/cdi/impl/MemorySpaceCacheTest.java
+++ test/org/openlcb/cdi/impl/MemorySpaceCacheTest.java
@@ -14,26 +14,29 @@ import org.openlcb.*;
  */
 public class MemorySpaceCacheTest {
 
+    private OlcbInterface oi;
+    private NodeID nodeID;
+ 
     @Test
     public void testCTor() {
-        NodeID nodeID = new NodeID(new byte[]{1,2,3,4,5,6});
-        Connection testConnection = new AbstractConnection(){
-            public void put(Message msg, Connection node) {
-            }
-        };
-        OlcbInterface oi = new OlcbInterface(nodeID,testConnection);
         MemorySpaceCache t = new MemorySpaceCache(oi,nodeID,42);
         Assert.assertNotNull("exists",t);
-        oi.dispose();
     }
 
     // The minimal setup for log4J
     @Before
     public void setUp() {
+        nodeID = new NodeID(new byte[]{1,2,3,4,5,6});
+        Connection testConnection = new AbstractConnection(){
+            public void put(Message msg, Connection node) {
+            }
+        };
+        oi = new OlcbInterface(nodeID,testConnection);
     }
 
     @After
     public void tearDown() {
+        oi.dispose();
     }
 
 }
