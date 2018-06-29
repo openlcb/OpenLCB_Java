d9bef87dc47f088cc7f5615d98b84e5c2d257d5f
Balazs Racz <balazs.racz@gmail.com>
2018-06-29 18:18:23 +0200

    Factors out fake ReadWriteAccess from the swing codebase because it is used in the core implementation as well.

--- src/org/openlcb/cdi/swing/CdiPanel.java
+++ src/org/openlcb/cdi/swing/CdiPanel.java
@@ -1601,22 +1601,6 @@ public class CdiPanel extends JPanel {
     }
 
     /**
-      * Provide access to e.g. a MemoryConfig service.
-     *
-      * Default just writes output for debug
-      */
-    public static class ReadWriteAccess {
-        public void doWrite(long address, int space, byte[] data, final
-                            MemoryConfigurationService.McsWriteHandler handler) {
-            logger.log(Level.FINE, "Write to {0} in space {1}", new Object[]{address, space});
-        }
-        public void doRead(long address, int space, int length, final MemoryConfigurationService
-                .McsReadHandler handler) {
-            logger.log(Level.FINE, "Read from {0} in space {1}", new Object[]{address, space});
-        }
-    }
-
-    /**
      * Handle GUI hook requests if needed
      *
      * Default behavior is to do nothing
