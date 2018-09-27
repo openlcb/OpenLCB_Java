20a9e89c44a54ebce2b351bc47f6f4c36ed6c0b7
Paul Bender <paul.bender@acm.org>
2018-09-26 21:12:37 -0400

    convert some tests to JUnit4; remove a test package from all test that caused it to be run twice

--- test/org/openlcb/PackageTest.java
+++ test/org/openlcb/PackageTest.java
@@ -1,107 +1,66 @@
 package org.openlcb;
 
-import junit.framework.Assert;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import junit.framework.JUnit4TestAdapter;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        UtilitiesTest.class,
+        VersionTest.class,
+        EventIDTest.class,
+        NodeIDTest.class,
+        MessageTypeIdentifierTest.class,
+        MessageTest.class,
+        MessageDecoderTest.class,
+        NodeTest.class,
+        // specific message types (uses Node, IDs)
+        InitializationCompleteMessageTest.class,
+        OptionalIntRejectedMessageTest.class,
+        VerifiedNodeIDNumberMessageTest.class,
+        VerifyNodeIDNumberMessageTest.class,
+        IdentifyProducersMessageTest.class,
+        ProducerIdentifiedMessageTest.class,
+        IdentifyConsumersMessageTest.class,
+        ConsumerIdentifiedMessageTest.class,
+        IdentifyEventsMessageTest.class,
+        ProducerConsumerEventReportMessageTest.class,
+        LearnEventMessageTest.class,
+        DatagramMessageTest.class,
+        ProtocolIdentificationRequestMessageTest.class,
+        ProtocolIdentificationReplyMessageTest.class,
+        ProtocolIdentificationTest.class,
+        SimpleNodeIdentInfoRequestMessageTest.class,
+        SimpleNodeIdentInfoReplyMessageTest.class,
+        SimpleNodeIdentTest.class,
+        ConfigurationPortalTest.class,
+        SingleLinkNodeTest.class,
+        GatewayTest.class,
+        ThrottleTest.class,
+        MimicNodeStoreTest.class,
+        LoaderClientTest.class,
+        CommonIdentifiersTest.class,       
+        DatagramAcknowledgedMessageTest.class,
+        DatagramRejectedMessageTest.class,  
+        DefaultPropertyListenerSupportTest.class,	
+        OlcbInterfaceTest.class, 
+        StreamDataCompleteMessageTest.class,       
+        StreamDataProceedMessageTest.class,       
+        StreamDataSendMessageTest.class,    
+        StreamInitiateReplyMessageTest.class,       
+        StreamInitiateRequestMessageTest.class,  
+        // test implementation classes
+        org.openlcb.implementations.PackageTest.class,
+        org.openlcb.messages.PackageTest.class,
+        org.openlcb.swing.PackageTest.class,
+        org.openlcb.cdi.PackageTest.class,
+        org.openlcb.hub.PackageTest.class,       
+        org.openlcb.protocols.PackageTest.class,       
+        // test CAN classes
+        org.openlcb.can.PackageTest.class
+})
 /**
  * @author  Bob Jacobsen   Copyright 2009, 2012
  */
-public class PackageTest extends TestCase {
-    public void testStart() {
-    }
-    
-    // from here down is testing infrastructure
-    
-    public PackageTest(String s) {
-        super(s);
-    }
-
-    // Main entry point
-    static public void main(String[] args) {
-        String[] testCaseName = {PackageTest.class.getName()};
-        junit.textui.TestRunner.main(testCaseName);
-    }
-
-    // test suite from all defined tests
-    public static Test suite() {
-        TestSuite suite = new TestSuite(PackageTest.class);
-
-        suite.addTest(UtilitiesTest.suite());
-        suite.addTest(VersionTest.suite());
-
-        suite.addTest(EventIDTest.suite());
-        suite.addTest(NodeIDTest.suite());
-
-        suite.addTest(MessageTypeIdentifierTest.suite());
-
-        suite.addTest(MessageTest.suite());
-
-        suite.addTest(MessageDecoderTest.suite());
-        suite.addTest(NodeTest.suite());
-
-        // specific message types (uses Node, IDs)
-        suite.addTest(InitializationCompleteMessageTest.suite());
-
-        suite.addTest(OptionalIntRejectedMessageTest.suite());
-
-        suite.addTest(VerifiedNodeIDNumberMessageTest.suite());
-        suite.addTest(VerifyNodeIDNumberMessageTest.suite());
-
-        suite.addTest(IdentifyProducersMessageTest.suite());
-        suite.addTest(ProducerIdentifiedMessageTest.suite());
-        suite.addTest(IdentifyConsumersMessageTest.suite());
-        suite.addTest(ConsumerIdentifiedMessageTest.suite());
-        suite.addTest(IdentifyEventsMessageTest.suite());
-        suite.addTest(ProducerConsumerEventReportMessageTest.suite());
-
-        suite.addTest(LearnEventMessageTest.suite());
-
-        suite.addTest(DatagramMessageTest.suite());
-
-        suite.addTest(ProtocolIdentificationRequestMessageTest.suite());
-        suite.addTest(ProtocolIdentificationReplyMessageTest.suite());
-        suite.addTest(ProtocolIdentificationTest.suite());
-        
-        suite.addTest(SimpleNodeIdentInfoRequestMessageTest.suite());
-        suite.addTest(SimpleNodeIdentInfoReplyMessageTest.suite());
-        suite.addTest(SimpleNodeIdentTest.suite());
-
-        suite.addTest(ConfigurationPortalTest.suite());
-
-        suite.addTest(SingleLinkNodeTest.suite());
-
-        suite.addTest(GatewayTest.suite());
-
-        suite.addTest(ThrottleTest.suite());
-
-        suite.addTest(MimicNodeStoreTest.suite());
-        
-        suite.addTest(LoaderClientTest.suite());
-        suite.addTest(new JUnit4TestAdapter(CommonIdentifiersTest.class));       
-        suite.addTest(new JUnit4TestAdapter(DatagramAcknowledgedMessageTest.class));       
-        suite.addTest(new JUnit4TestAdapter(DatagramRejectedMessageTest.class));       
-        suite.addTest(new JUnit4TestAdapter(DefaultPropertyListenerSupportTest.class));       
-        suite.addTest(new JUnit4TestAdapter(OlcbInterfaceTest.class));       
-        suite.addTest(new JUnit4TestAdapter(StreamDataCompleteMessageTest.class));       
-        suite.addTest(new JUnit4TestAdapter(StreamDataProceedMessageTest.class));       
-        suite.addTest(new JUnit4TestAdapter(StreamDataSendMessageTest.class));       
-        suite.addTest(new JUnit4TestAdapter(StreamInitiateReplyMessageTest.class));       
-        suite.addTest(new JUnit4TestAdapter(StreamInitiateRequestMessageTest.class));       
-
-        // test implementation classes
-        suite.addTest(org.openlcb.implementations.PackageTest.suite());
-        suite.addTest(org.openlcb.messages.PackageTest.suite());
-        suite.addTest(org.openlcb.swing.PackageTest.suite());
-        suite.addTest(org.openlcb.cdi.PackageTest.suite());
-        suite.addTest(new JUnit4TestAdapter(org.openlcb.hub.PackageTest.class));       
-        suite.addTest(new JUnit4TestAdapter(org.openlcb.protocols.PackageTest.class));       
-        
-        // test CAN classes
-        suite.addTest(org.openlcb.can.PackageTest.suite());
-
-        return suite;
-    }
+public class PackageTest {
 }
--- test/org/openlcb/SimpleNodeIdentInfoReplyMessageTest.java
+++ test/org/openlcb/SimpleNodeIdentInfoReplyMessageTest.java
@@ -1,20 +1,18 @@
 package org.openlcb;
 
-import junit.framework.Assert;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.*;
 
 /**
  * @author  Bob Jacobsen   Copyright 2012
  * @version $Revision$
  */
-public class SimpleNodeIdentInfoReplyMessageTest extends TestCase {
+public class SimpleNodeIdentInfoReplyMessageTest {
     boolean result;
     
     NodeID nodeID1 = new NodeID(new byte[]{1,2,3,4,5,6});
     NodeID nodeID2 = new NodeID(new byte[]{0,0,0,0,0,0});
 
+    @Test
     public void testEqualsSame() {
         Message m1 = new SimpleNodeIdentInfoReplyMessage(
                                nodeID1, nodeID2, new byte[]{1,2});
@@ -24,6 +22,7 @@ public class SimpleNodeIdentInfoReplyMessageTest extends TestCase {
         Assert.assertTrue(m1.equals(m2));
     }
 
+    @Test
     public void testNotEqualsDifferentNode() {
         Message m1 = new SimpleNodeIdentInfoReplyMessage(
                                 nodeID1, nodeID2, new byte[]{1,2});
@@ -34,6 +33,7 @@ public class SimpleNodeIdentInfoReplyMessageTest extends TestCase {
     }
 
 
+    @Test
     public void testNotEqualsDifferentValue() {
         Message m1 = new SimpleNodeIdentInfoReplyMessage(
                                 nodeID1, nodeID2, new byte[]{1,2});
@@ -43,6 +43,7 @@ public class SimpleNodeIdentInfoReplyMessageTest extends TestCase {
         Assert.assertTrue( ! m1.equals(m2));
     }
 
+    @Test
     public void testNotEqualsDifferentValueLength() {
         Message m1 = new SimpleNodeIdentInfoReplyMessage(
                                 nodeID1, nodeID2, new byte[]{1,2});
@@ -51,6 +52,8 @@ public class SimpleNodeIdentInfoReplyMessageTest extends TestCase {
     
         Assert.assertTrue( ! m1.equals(m2));
     }
+
+    @Test
     public void testNotEqualsDifferentValueLengthBis() {
         Message m1 = new SimpleNodeIdentInfoReplyMessage(
                                 nodeID1, nodeID2, new byte[]{1,2,3});
@@ -60,7 +63,7 @@ public class SimpleNodeIdentInfoReplyMessageTest extends TestCase {
         Assert.assertTrue( ! m1.equals(m2));
     }
 
-
+    @Test
     public void testHandling() {
         result = false;
         Node n = new Node(){
@@ -76,29 +79,12 @@ public class SimpleNodeIdentInfoReplyMessageTest extends TestCase {
         Assert.assertTrue(result);
     }
 
+    @Test
     public void testPrint() {
         Message m = new SimpleNodeIdentInfoReplyMessage(nodeID1, nodeID2, new byte[]{4,'a','b',
                 'c',0,'d','e','f','g',0,0,2,'u',0,'v',0});
         String s = m.toString();
         Assert.assertEquals("01.02.03.04.05.06 - 00.00.00.00.00.00 Simple Node Ident Info with" +
                 " content '4,abc,defg,,2,u,v,'", s);
-    }
-
-    // from here down is testing infrastructure
-    
-    public SimpleNodeIdentInfoReplyMessageTest(String s) {
-        super(s);
-    }
-
-    // Main entry point
-    static public void main(String[] args) {
-        String[] testCaseName = {SimpleNodeIdentInfoReplyMessageTest.class.getName()};
-        junit.textui.TestRunner.main(testCaseName);
-    }
-
-    // test suite from all defined tests
-    public static Test suite() {
-        TestSuite suite = new TestSuite(SimpleNodeIdentInfoReplyMessageTest.class);
-        return suite;
     }
 }
