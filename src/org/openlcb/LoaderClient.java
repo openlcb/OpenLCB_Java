6ac8772ea491c6efeb248cc7edfce4b1589cbc74
Paul Bender <paul.bender@acm.org>
2019-04-24 11:43:52 -0400

    revert static timer in LoaderClient

--- src/org/openlcb/LoaderClient.java
+++ src/org/openlcb/LoaderClient.java
@@ -54,9 +54,7 @@ public class LoaderClient extends MessageDecoder {
         connection = _connection;
         dcs = _dcs;
         mcs = _mcs;
-	if( timer == null ) { 
-           timer = new Timer("OpenLCB LoaderClient Timeout Timer");
-        }
+        timer = new Timer("OpenLCB LoaderClient Timeout Timer");
     }
 
     /* Protocol:
@@ -117,7 +115,7 @@ public class LoaderClient extends MessageDecoder {
         startTimeout(FREEZE_REBOOT_TIMEOUT_MSEC);
     }
 
-    private static Timer timer;
+    private Timer timer;
     private TimerTask task = null;
     private void startTimeout(int period_msec) {
         task = new TimerTask(){
