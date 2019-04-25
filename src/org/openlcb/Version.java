c2146b555d0cacf2edfcd2600770fc93f878fd7a
Paul Bender <paul.bender@acm.org>
2019-04-25 00:02:21 -0400

    update version to 0.7.21

--- src/org/openlcb/Version.java
+++ src/org/openlcb/Version.java
@@ -34,7 +34,7 @@ public class Version {
 
     /* Library modifier - updated periodically
      */
-    static final public int libMod = 20;
+    static final public int libMod = 21;
 
     /**
      * Checks if the current specification version is above a specific threshold.
