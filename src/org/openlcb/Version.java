5b2b068761329408bd9b23a582f5bdce233b8d62
Balazs Racz <balazs.racz@gmail.com>
2021-03-27 22:17:10 +0100

    Build version 0.7.28 (#186)

--- src/org/openlcb/Version.java
+++ src/org/openlcb/Version.java
@@ -34,7 +34,7 @@ public class Version {
 
     /* Library modifier - updated periodically
      */
-    static final public int libMod = 27;
+    static final public int libMod = 28;
 
     /**
      * Checks if the current specification version is above a specific threshold.
