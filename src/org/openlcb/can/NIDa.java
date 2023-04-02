66806e5cd6d671de24561801b77b660f7af21a25
Bob Jacobsen <jacobsen@mac.com>
2023-04-02 19:27:16 -0400

    correct PRNG calculation

--- src/org/openlcb/can/NIDa.java
+++ src/org/openlcb/can/NIDa.java
@@ -8,18 +8,18 @@ import org.openlcb.*;
  * This class merely does computations, and doesn't control
  * sending and receiving of CAN frames.
  *
- * 
+ *
  * @author  Bob Jacobsen   Copyright 2009, 2010
  * @version $Revision$
  */
 public class NIDa {
 
     NodeID nid;
-    
+
     public NIDa(NodeID nid) {
         // initialize seed from NodeID
         loadSeed(nid);
-        
+
         // step the generator once and use result
         nextAlias();
 }
@@ -29,14 +29,14 @@ public class NIDa {
     /**
      * Update to the next alias in the series
      */
-    public void nextAlias() { 
+    public void nextAlias() {
         stepGenerator();
         nida = computeAliasFromGenerator();
     }
-    
+
     // temporary storage of current alias value
     int nida = 0;
-    
+
     /**
      * For use in testing subclasses only,
      * this forces the current seed to a specific
@@ -60,7 +60,7 @@ public class NIDa {
     protected void forceAliasValue(int alias) {
         nida = alias;
     }
-   
+
     /**
      * Algorithm to produce seed from the six bytes
      * of a node ID.
@@ -75,22 +75,22 @@ public class NIDa {
         lfsr1 = (((long)val[0]) << 16) | (((long)val[1]) << 8) | ((long)val[2]);
         lfsr2 = (((long)val[3]) << 16) | (((long)val[4]) << 8) | ((long)val[5]);
     }
-    
+
     /**
      * Advance the sequence generator by one step.
      */
-    protected void stepGenerator() {   
+    protected void stepGenerator() {
        // step the PRNG
        // First, form 2^9*val
        long temp1 = ((lfsr1<<9) | ((lfsr2>>15)&0x1FF)) & 0xFFFFFF;
        long temp2 = (lfsr2<<9) & 0xFFFFFF;
-       
+
        // add
        lfsr2 = lfsr2 + temp2 + 0x7A4BA9l;
        lfsr1 = lfsr1 + temp1 + 0x1B0CA3l;
-       
+
        // carry
-       lfsr1 = (lfsr1 & 0xFFFFFF) | ((lfsr2&0xFF000000) >> 24);
+       lfsr1 = (lfsr1 & 0xFFFFFF) + ((lfsr2&0xFF000000) >> 24);
        lfsr2 = lfsr2 & 0xFFFFFF;
      }
 
