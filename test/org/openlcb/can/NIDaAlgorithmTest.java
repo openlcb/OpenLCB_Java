0e32ff1c6681ec9527d60fbccff45d1ff652f150
Bob Jacobsen <jacobsen@mac.com>
2023-08-22 05:40:02 -0400

    Send AMD at end of alias sequence (#224)
    Fix for #121.
    
    ===
    
    * Send AMD at end of alias sequence
    
    * fix complete, AMD sending in timeout
    
    * test for complete state updated

--- test/org/openlcb/can/NIDaAlgorithmTest.java
+++ test/org/openlcb/can/NIDaAlgorithmTest.java
@@ -52,15 +52,17 @@ public class NIDaAlgorithmTest {
     }
 
     @Test
-    public void testFifth() {
+    public void testSixth() {
         Assert.assertTrue("not complete", !alg.isComplete());
 
-        // seventh frame is RIM
+        // fifth frame is RIM
         Assert.assertTrue(alg.nextFrame().isCIM());
         Assert.assertTrue(alg.nextFrame().isCIM());
         Assert.assertTrue(alg.nextFrame().isCIM());
         Assert.assertTrue(alg.nextFrame().isCIM());
         Assert.assertTrue(alg.nextFrame().isRIM());
+
+        Assert.assertNotEquals(alg.nextFrame(), null); // AMD frame
 
         Assert.assertTrue("complete", alg.isComplete());
 
@@ -93,6 +95,9 @@ public class NIDaAlgorithmTest {
         t.setCIM(1, 0, 0);
         alg.processFrame(t);
         Assert.assertTrue(alg.nextFrame().isRIM());
+
+        // sixth is AMD
+        Assert.assertNotEquals((alg.nextFrame()), null);
 
         Assert.assertTrue("complete", alg.isComplete());
 
@@ -130,6 +135,9 @@ public class NIDaAlgorithmTest {
         f = alg.nextFrame();
         Assert.assertTrue(f.isRIM());
 
+        // sixth is AMD
+        Assert.assertNotEquals((f = alg.nextFrame()), null);
+
         Assert.assertTrue("complete", alg.isComplete());
 
         Assert.assertEquals((f = alg.nextFrame()), null);
@@ -156,6 +164,9 @@ public class NIDaAlgorithmTest {
         // fifth
         f = alg.nextFrame();
         Assert.assertTrue(f.isRIM());
+
+        // sixth is AMD
+        Assert.assertNotEquals((f = alg.nextFrame()), null);
 
         int nida = f.getSourceAlias();
         Assert.assertTrue("complete", alg.isComplete());
@@ -188,7 +199,7 @@ public class NIDaAlgorithmTest {
         alg1.nextFrame();
         alg1.nextFrame();
 
-        int expectedCount = 5;
+        int expectedCount = 6;
         int count = sequentialRunner(new NIDaAlgorithm[]{alg1, alg2}, expectedCount);
 
         debug("tSS2 converges " + count);
